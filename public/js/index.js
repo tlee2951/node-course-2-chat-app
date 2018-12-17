bba1534411c1a26314ef4654d59003b86e474045
Tom Lee <tlee2951@gmail.com>
2018-12-17 06:24:01 -0700

    Remove obsolete code

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on("connect", function() {
   console.log("Connected to server");
 
-  socket.emit("createEmail", {
-    to: "jen@example.com",
-    text: "Hey. This is Andrew"
-  });
-
   socket.emit("createMessage", {
     from: "Hubba bubba",
     text: "Suck it Billy Bob!"
@@ -16,10 +11,6 @@ socket.on("connect", function() {
 
 socket.on("disconnect", function() {
   console.log("Disconnected from server");
-});
-
-socket.on("newEmail", function(email) {
-  console.log("New email", email);
 });
 
 socket.on("newMessage", function(msg) {
