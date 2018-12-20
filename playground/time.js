e8270206bcfb694c9d6f364aee91760a4a58f9c7
Tom Lee <tlee2951@gmail.com>
2018-12-20 08:58:30 -0700

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require("moment");
+
+// var date = moment();
+// date.add(100, "year").subtract(13, "months");
+// console.log(date.format("MMM Do, YYYY"));
+
+var date = moment();
+console.log(date.format("h:mm a"));
--- public/js/index.js
+++ public/js/index.js
@@ -9,19 +9,19 @@ socket.on("disconnect", function() {
 });
 
 socket.on("newMessage", function(msg) {
-  console.log("Got new message", msg);
+  var formattedTime = moment(msg.createdAt).format("h:mm a");
   var li = jQuery("<li></li>");
-  li.text(`${msg.from}: ${msg.text}`);
+  li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
 
   jQuery("#messages").append(li);
 });
 
 socket.on("newLocationMessage", function(msg) {
-  console.log("Got new location message", msg);
+  var formattedTime = moment(msg.createdAt).format("h:mm a");
   var li = jQuery("<li></li>");
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${msg.from}: `);
+  li.text(`${msg.from} ${formattedTime}: `);
   a.attr("href", msg.url);
   li.append(a);
   jQuery("#messages").append(li);
